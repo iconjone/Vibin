04cf62164583208742050f33d04a81b05e2a127c
Jonathan <jone.bala@gmail.com>
2022-08-18 14:13:02 -0500

    Bluetooth Connection Successful

--- Bluetooth-Sink/src/main.cpp
+++ Bluetooth-Sink/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include "AudioTools.h"
+#include "AudioLibs/AudioA2DP.h"
+
+
+
+A2DPStream in = A2DPStream::instance() ; // A2DP input - A2DPStream is a singleton!
+CsvStream<int16_t> out(Serial, 2); // ASCII stream as csv 
+StreamCopy copier(out, in); // copy in to out
+
+// Arduino Setup
+void setup(void) {
+  Serial.begin(115200);
+
+  // start the bluetooth audio receiver
+  Serial.println("starting A2DP...");
+  auto cfg = in.defaultConfig(RX_MODE);
+  cfg.name = "VibinChair";
+  in.begin(cfg);  
 }
 
+// Arduino loop  
 void loop() {
-  // put your main code here, to run repeatedly:
+  copier.copy();
 }
