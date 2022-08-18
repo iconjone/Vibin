77d04e4a3468bba55ed5f1cebb60d85d3001876b
Jonathan <jone.bala@gmail.com>
2022-08-18 15:59:31 -0500

    sinewave generator

--- Bluetooth-Sink/src/main.cpp
+++ Bluetooth-Sink/src/main.cpp
@@ -1,39 +1,38 @@
 #include <Arduino.h>
-
 #include "AudioTools.h"
-#include "AudioLibs/AudioA2DP.h"
 
-
-
-A2DPStream in = A2DPStream::instance() ; // A2DP input - A2DPStream is a singleton!
-I2SStream out; 
 uint16_t sample_rate=44100;
-uint8_t channels = 2;      
-//CsvStream<int16_t> out(Serial, 2); // ASCII stream as csv 
-StreamCopy copier(out, in); // copy in to out
+uint8_t channels = 2;                                      // The stream will have 2 channels 
+SineWaveGenerator<int16_t> sineWave(32000);                // subclass of SoundGenerator with max amplitude of 32000
+GeneratedSoundStream<int16_t> sound(sineWave);             // Stream generated from sine wave
+I2SStream out; 
+StreamCopy copier(out, sound);                             // copies sound into i2s
 
-
-void setup(void) {
+// Arduino Setup
+void setup(void) {  
+  // Open Serial 
   Serial.begin(115200);
+  while(!Serial);
+  AudioLogger::instance().begin(Serial, AudioLogger::Info);
 
-
-  // start the bluetooth audio receiver
-  Serial.println("starting A2DP...");
-  auto cfg = in.defaultConfig(RX_MODE);
-  cfg.name = "VibinChair";
-  in.begin(cfg);  
-    // start I2S
+  // start I2S
   Serial.println("starting I2S...");
   auto config = out.defaultConfig(TX_MODE);
   config.sample_rate = sample_rate; 
   config.channels = channels;
   config.bits_per_sample = 16;
-
+  config.i2s_format = I2S_STD_FORMAT;
+  config.pin_bck = 26;
+  config.pin_ws = 25;
+  config.pin_data = 22;
   out.begin(config);
 
+  // Setup sine wave
+  sineWave.begin(channels, sample_rate, N_B4);
+  Serial.println("started...");
 }
 
-
+// Arduino loop - copy sound to out 
 void loop() {
   copier.copy();
 }
