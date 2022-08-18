17bb3d3c2750e9d10dd8d219e75439da3133f26c
Jonathan <jone.bala@gmail.com>
2022-08-18 15:19:19 -0500

    Bluetooth -> i2s

--- Bluetooth-Sink/src/main.cpp
+++ Bluetooth-Sink/src/main.cpp
@@ -6,21 +6,34 @@
 
 
 A2DPStream in = A2DPStream::instance() ; // A2DP input - A2DPStream is a singleton!
-CsvStream<int16_t> out(Serial, 2); // ASCII stream as csv 
+I2SStream out; 
+uint16_t sample_rate=44100;
+uint8_t channels = 2;      
+//CsvStream<int16_t> out(Serial, 2); // ASCII stream as csv 
 StreamCopy copier(out, in); // copy in to out
 
-// Arduino Setup
+
 void setup(void) {
   Serial.begin(115200);
+
 
   // start the bluetooth audio receiver
   Serial.println("starting A2DP...");
   auto cfg = in.defaultConfig(RX_MODE);
   cfg.name = "VibinChair";
   in.begin(cfg);  
+    // start I2S
+  Serial.println("starting I2S...");
+  auto config = out.defaultConfig(TX_MODE);
+  config.sample_rate = sample_rate; 
+  config.channels = channels;
+  config.bits_per_sample = 16;
+
+  out.begin(config);
+
 }
 
-// Arduino loop  
+
 void loop() {
   copier.copy();
 }
