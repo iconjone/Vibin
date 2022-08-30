cfbd3710682063722e0cbe8380790698c672c5a7
Jonathan <jone.bala@gmail.com>
2022-08-30 11:25:09 -0500

    Working Websockets

--- Bluetooth-Sink/src/main.cpp
+++ Bluetooth-Sink/src/main.cpp
@@ -24,11 +24,81 @@ MAX30105 particleSensor;
 TaskHandle_t I2CTaskHandle;
 
 AsyncWebServer httpServer(80);
+AsyncWebSocket ws("/ws");
 
 // setup led pin 2
 const int ledPin = 2;
 
 bool ledState = false;
+
+void onEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+  if(type == WS_EVT_CONNECT){
+    //client connected
+    //os_printf("ws[%s][%u] connect\n", server->url(), client->id());
+    client->printf("Hello Client %u :)", client->id());
+    client->ping();
+  } else if(type == WS_EVT_DISCONNECT){
+    //client disconnected
+    //os_printf("ws[%s][%u] disconnect: %u\n", server->url(), client->id());
+  } else if(type == WS_EVT_ERROR){
+    //error was received from the other end
+    //os_printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+  } else if(type == WS_EVT_PONG){
+    //pong message was received (in response to a ping request maybe)
+    //os_printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
+  } else if(type == WS_EVT_DATA){
+    //data packet
+    AwsFrameInfo * info = (AwsFrameInfo*)arg;
+    if(info->final && info->index == 0 && info->len == len){
+      //the whole message is in a single frame and we got all of it's data
+      //os_printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
+      if(info->opcode == WS_TEXT){
+        data[len] = 0;
+        //os_printf("%s\n", (char*)data);
+      } else {
+        for(size_t i=0; i < info->len; i++){
+          //os_printf("%02x ", data[i]);
+        }
+        //os_printf("\n");
+      }
+      if(info->opcode == WS_TEXT)
+        client->text("I got your text message");
+      else
+        client->binary("I got your binary message");
+    } else {
+      //message is comprised of multiple frames or the frame is split into multiple packets
+      if(info->index == 0){
+        if(info->num == 0)
+          Serial.println("Pretty big");
+          //os_printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+        //os_printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+      }
+
+      //os_printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT)?"text":"binary", info->index, info->index + len);
+      if(info->message_opcode == WS_TEXT){
+        data[len] = 0;
+        //os_printf("%s\n", (char*)data);
+      } else {
+        for(size_t i=0; i < len; i++){
+         // os_printf("%02x ", data[i]);
+        }
+       // os_printf("\n");
+      }
+
+      if((info->index + len) == info->len){
+        //os_printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+        if(info->final){
+          //os_printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+          if(info->message_opcode == WS_TEXT)
+            client->text("I got your text message");
+          else
+            client->binary("I got your binary message");
+        }
+      }
+    }
+  }
+}
+
 void I2CTask(void *pvParameters)
 {
   pinMode(ledPin, OUTPUT);
@@ -37,6 +107,11 @@ void I2CTask(void *pvParameters)
   Serial.println("WiFi started");
   // print ip address
   Serial.println(WiFi.localIP());
+
+
+
+  ws.onEvent(onEvent);
+  httpServer.addHandler(&ws);
 
   httpServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
                 {
@@ -98,8 +173,10 @@ void I2CTask(void *pvParameters)
 
     // Serial.print("IR=");
     // Serial.print(irValue);
-    Serial.print(", BPM=");
-    Serial.println(beatsPerMinute);
+    if(beatsPerMinute > 30){
+      Serial.print("BPM=");
+      Serial.println(beatsPerMinute);
+    }
     // Serial.print(", Avg BPM=");
     // Serial.print(beatAvg);
 
