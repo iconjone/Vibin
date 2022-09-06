f5bbd60f73df582b7870a58dbb22b910e5063020
Jonathan <jone.bala@gmail.com>
2022-09-06 11:41:44 -0500

    Get Handle for Source, change to i2s

--- Bluetooth-Source/src/main.cpp
+++ Bluetooth-Source/src/main.cpp
@@ -8,7 +8,9 @@
 I2SStream i2sStream;                            // Access I2S as stream
 A2DPStream a2dpStream = A2DPStream::instance(); // access A2DP as stream
 VolumeStream volume(a2dpStream);
-StreamCopy copier(volume, i2sStream); // copy i2sStream to a2dpStream
+SineWaveGenerator<int16_t> sineWave(32000);                // subclass of SoundGenerator with max amplitude of 32000
+GeneratedSoundStream<int16_t> sound(sineWave);             // Stream generated from sine wave
+StreamCopy copier(volume, sound); // copy i2sStream to a2dpStream
 const int ledPin = 2;
 int mutePin = 19;
 int EnPin = 18;
@@ -34,31 +36,40 @@ void setup() {
   digitalWrite(m6Pin, HIGH);
   digitalWrite(m7Pin, HIGH);
 
-  volume.setVolume(0.3);
+
+
+
+        // start i2s input with default configuration
+  //   Serial.println("starting I2S...");
+  //   a2dpStream.setNotifyAudioChange(i2sStream); // i2s is using the info from a2dp
+  //   auto config = i2sStream.defaultConfig(RX_MODE);
+    
+
+  // config.sample_rate = sample_rate;
+  // config.channels = channels;
+  // config.bits_per_sample = 16;
+  // config.pin_bck = 25;
+  // config.pin_ws = 27;
+  // config.pin_data = 32;
+
+  // i2sStream.begin(config);
+
+    volume.setVolume(1);
       Serial.println("starting A2DP...");
     auto cfgA2DP = a2dpStream.defaultConfig(TX_MODE);
-    cfgA2DP.name = "JBL GO 2";
+    //BluetoothA2DPSource *a2dp_source = a2dpStream.source;
+    auto ad2p_src = a2dpStream.source();
+
+    cfgA2DP.name = "Jonathan Samuel's Beats Studio³";
+    Serial.print("Connecting to ");
+    Serial.println(cfgA2DP.name);
     cfgA2DP.auto_reconnect = true;
 
     //init a char array to be passed as a pointer to begin()
     //char *name = new char[cfgA2DP.name.length() + 1];
     a2dpStream.begin(cfgA2DP);
 
-
-        // start i2s input with default configuration
-    Serial.println("starting I2S...");
-    a2dpStream.setNotifyAudioChange(i2sStream); // i2s is using the info from a2dp
-    auto config = i2sStream.defaultConfig(RX_MODE);
-    
-
-  config.sample_rate = sample_rate;
-  config.channels = channels;
-  config.bits_per_sample = 16;
-  config.pin_bck = 4;
-  config.pin_ws = 5;
-  config.pin_data = 19;
-
-  i2sStream.begin(config);
+    sineWave.begin(channels, sample_rate, N_B4);
 }
 
 void loop() {
