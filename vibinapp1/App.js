5ba4757ac1592a5a305d3ae75089b7cf2c6498ed
Jonathan <jone.bala@gmail.com>
2022-09-10 19:48:07 -0500

    WORKING TO WEB

---
+++ vibinapp1/App.js
@@ -0,0 +1,49 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, SafeAreaView } from "react-native";
+import React from "react";
+import * as eva from "@eva-design/eva";
+import {
+  ApplicationProvider,
+  Layout,
+  Text,
+  Toggle,
+} from "@ui-kitten/components";
+
+const ToggleSimpleUsageShowcase = () => {
+  const [checked, setChecked] = React.useState(false);
+
+  const onCheckedChange = (isChecked) => {
+    setChecked(isChecked);
+  };
+
+  return (
+    <Toggle checked={checked} onChange={onCheckedChange}>
+      {`Checked: ${checked}`}
+    </Toggle>
+  );
+};
+
+const HomeScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text category="h1"> Connect to Vibin</Text>
+
+    <ToggleSimpleUsageShowcase />
+  </Layout>
+);
+
+export default function App() {
+  return (
+    <ApplicationProvider {...eva} theme={eva.dark}>
+      <HomeScreen />
+    </ApplicationProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
